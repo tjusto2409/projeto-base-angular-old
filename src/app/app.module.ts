ddd52d4a1be18646d792e121baa498b6be92f226
Thiago Nogueira Justo <thiago.j.terceiro@brasilcap.com.br>
2019-12-11 17:48:16 -0300

    Importando primeiro modulo em modulo de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { FirstModuleModule } from './modules/first-module/first-module.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    FirstModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
